59da0444910f1f841ee00c6e71e4fa99c5e9512f
Kayra Uylar <k.uylar@outlook.com>
2023-05-07 15:07:55 +0300

    feat(player): update bottom sheet text

--- app/src/main/java/dev/kuylar/lighttube/ui/VideoPlayerManager.kt
+++ app/src/main/java/dev/kuylar/lighttube/ui/VideoPlayerManager.kt
@@ -8,18 +8,23 @@ import dev.kuylar.lighttube.R
 import dev.kuylar.lighttube.api.LightTubeApi
 import dev.kuylar.lighttube.ui.activity.MainActivity
 import android.os.Handler
+import android.widget.TextView
 import androidx.fragment.app.FragmentContainerView
+import com.google.android.exoplayer2.Player
 import dev.kuylar.lighttube.ui.fragment.VideoInfoFragment
 import kotlin.concurrent.thread
 
-class VideoPlayerManager(activity: MainActivity) {
+class VideoPlayerManager(activity: MainActivity) : Player.Listener {
 	private val exoplayerView: StyledPlayerView = activity.findViewById(R.id.player)
 	private val player: ExoPlayer = ExoPlayer.Builder(activity).build()
 	private val api: LightTubeApi = activity.api
 	private val fragmentManager = activity.supportFragmentManager
+	private val miniplayerTitle: TextView = activity.findViewById(R.id.miniplayer_video_title)
+	private val miniplayerSubtitle: TextView = activity.findViewById(R.id.miniplayer_video_subtitle)
 
 	init {
 		exoplayerView.player = player
+		player.addListener(this)
 	}
 
 	fun playVideo(id: String) {
@@ -44,5 +49,14 @@ class VideoPlayerManager(activity: MainActivity) {
 			setUri("${api.host}/proxy/media/$id.m3u8?useProxy=false")
 			setMediaMetadata(video.details.getMediaMetadata(api.host))
 		}.build()
+	}
+
+	override fun onEvents(player: Player, events: Player.Events) {
+		super.onEvents(player, events)
+
+		if (events.contains(Player.EVENT_MEDIA_ITEM_TRANSITION)) {
+			miniplayerTitle.text = player.currentMediaItem?.mediaMetadata?.title
+			miniplayerSubtitle.text = player.currentMediaItem?.mediaMetadata?.artist
+		}
 	}
 }
