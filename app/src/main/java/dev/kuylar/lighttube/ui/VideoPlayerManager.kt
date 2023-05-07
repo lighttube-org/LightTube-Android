aab0c213dfb89eba02408e27a8792cad2c3fc7b9
Kayra Uylar <k.uylar@outlook.com>
2023-05-07 19:34:25 +0300

    feat(player): implement controls & fullscreen

--- app/src/main/java/dev/kuylar/lighttube/ui/VideoPlayerManager.kt
+++ app/src/main/java/dev/kuylar/lighttube/ui/VideoPlayerManager.kt
@@ -1,32 +1,41 @@
 package dev.kuylar.lighttube.ui
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.ui.StyledPlayerView
-import dev.kuylar.lighttube.R
-import dev.kuylar.lighttube.api.LightTubeApi
-import dev.kuylar.lighttube.ui.activity.MainActivity
 import android.os.Handler
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import dev.kuylar.lighttube.R
+import dev.kuylar.lighttube.api.LightTubeApi
+import dev.kuylar.lighttube.ui.activity.MainActivity
 import dev.kuylar.lighttube.ui.fragment.VideoInfoFragment
 import kotlin.concurrent.thread
 
-class VideoPlayerManager(activity: MainActivity) : Player.Listener {
+class VideoPlayerManager(private val activity: MainActivity) : Player.Listener {
 	private val playerHandler: Handler
 	private val exoplayerView: StyledPlayerView = activity.findViewById(R.id.player)
+	private val fullscreenPlayer: StyledPlayerView = activity.findViewById(R.id.fullscreen_player)
 	private val player: ExoPlayer = ExoPlayer.Builder(activity).build()
 	private val api: LightTubeApi = activity.api
 	private val fragmentManager = activity.supportFragmentManager
+
 	private val miniplayerTitle: TextView = activity.findViewById(R.id.miniplayer_video_title)
 	private val miniplayerSubtitle: TextView = activity.findViewById(R.id.miniplayer_video_subtitle)
 	private val miniplayerProgress: ProgressBar =
 		activity.findViewById(R.id.miniplayer_progress_bar)
 	private val miniplayer: BottomSheetBehavior<View> = activity.miniplayer
+	private val playerControls = exoplayerView.findViewById<View>(R.id.player_controls)
+	private var fullscreen = false
 
 	init {
 		exoplayerView.player = player
@@ -48,6 +57,24 @@ class VideoPlayerManager(activity: MainActivity) : Player.Listener {
 			playerHandler.postDelayed(r, 100)
 		}
 		playerHandler.post(r)
+
+		// im sorry for this monstrosity too
+		initOnClickListeners(exoplayerView)
+		initOnClickListeners(fullscreenPlayer)
+	}
+
+	private fun initOnClickListeners(view: View) {
+		view.findViewById<MaterialButton>(R.id.player_play_pause).setOnClickListener {
+			if (player.isPlaying) player.pause() else player.play()
+		}
+
+		view.findViewById<MaterialButton>(R.id.player_fullscreen).setOnClickListener {
+			toggleFullscreen()
+		}
+	}
+
+	private fun getActivePlayerView(): StyledPlayerView {
+		return if (fullscreen) fullscreenPlayer else exoplayerView
 	}
 
 	fun playVideo(id: String) {
@@ -90,11 +117,64 @@ class VideoPlayerManager(activity: MainActivity) : Player.Listener {
 			miniplayerTitle.text = player.currentMediaItem?.mediaMetadata?.title
 			miniplayerSubtitle.text = player.currentMediaItem?.mediaMetadata?.artist
 		}
+
+		getActivePlayerView().findViewById<MaterialButton>(R.id.player_play_pause).icon =
+			AppCompatResources.getDrawable(
+				activity,
+				if (player.isPlaying) R.drawable.ic_pause else R.drawable.ic_play
+			)
+
+		getActivePlayerView().findViewById<CircularProgressIndicator>(R.id.player_buffering_progress).visibility =
+			if (player.playbackState == Player.STATE_BUFFERING) View.VISIBLE else View.GONE
 	}
 
 	fun stop() {
 		miniplayer.state = BottomSheetBehavior.STATE_HIDDEN
 		player.stop()
 		player.clearMediaItems()
+	}
+
+	fun toggleControls(visible: Boolean) {
+		playerControls.visibility = if (visible) View.VISIBLE else View.GONE
+	}
+
+	private fun toggleFullscreen() {
+		if (fullscreen) {
+			fullscreenPlayer.visibility = View.GONE
+			StyledPlayerView.switchTargetView(player, fullscreenPlayer, exoplayerView)
+			activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+			activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+			getActivePlayerView().findViewById<MaterialButton>(R.id.player_fullscreen).icon =
+				ContextCompat.getDrawable(
+					activity,
+					R.drawable.ic_fullscreen
+				)
+			fullscreen = false
+		} else {
+			fullscreenPlayer.visibility = View.VISIBLE
+			StyledPlayerView.switchTargetView(player, exoplayerView, fullscreenPlayer)
+			activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+			activity.window.decorView.systemUiVisibility = (
+					View.SYSTEM_UI_FLAG_FULLSCREEN
+							or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+							or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					)
+			getActivePlayerView().findViewById<MaterialButton>(R.id.player_fullscreen).icon =
+				ContextCompat.getDrawable(
+					activity,
+					R.drawable.ic_fullscreen_exit
+				)
+			fullscreen = true
+		}
+	}
+
+	fun exitFullscreen(): Boolean {
+		if (!fullscreen) return false
+		toggleFullscreen()
+		return true
+	}
+
+	fun setVolume(volume: Float) {
+		player.volume = volume
 	}
 }
--- app/src/main/java/dev/kuylar/lighttube/ui/activity/MainActivity.kt
+++ app/src/main/java/dev/kuylar/lighttube/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.navigation.findNavController
@@ -64,6 +65,13 @@ class MainActivity : AppCompatActivity() {
 		setupActionBarWithNavController(navController, appBarConfiguration)
 		navView.setupWithNavController(navController)
 
+		onBackPressedDispatcher.addCallback(this) {
+			if (!player.exitFullscreen()) // attempt to exit fullscreen
+				if (!minimizePlayer()) // attempt to minimize the player sheet
+					if (!navController.navigateUp()) // attempt to go back on the fragment history
+						finish() // close the app
+		}
+
 		val miniplayerView: View = findViewById(R.id.miniplayer)
 		miniplayerScene = miniplayerView.findViewById(R.id.miniplayer_motion)
 		miniplayer = BottomSheetBehavior.from(miniplayerView)
@@ -89,14 +97,27 @@ class MainActivity : AppCompatActivity() {
 			}
 
 			override fun onSlide(bottomSheet: View, slideOffset: Float) {
+				if (slideOffset < 0)
+					player.setVolume(max(0.1f, 1 + slideOffset * 3))
+				else
+					player.setVolume(1f)
 				miniplayerScene.progress = max(0f, min(1f, slideOffset * 5))
 
 				if (slideOffset > .3)
 					binding.navView.visibility = View.GONE
 				else
 					binding.navView.visibility = View.VISIBLE
+
+				player.toggleControls(slideOffset * 5 > 0.9)
 			}
 		})
+	}
+
+	private fun minimizePlayer(): Boolean {
+		return if (miniplayer.state == BottomSheetBehavior.STATE_EXPANDED) {
+			miniplayer.state = BottomSheetBehavior.STATE_COLLAPSED
+			true
+		} else false
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
