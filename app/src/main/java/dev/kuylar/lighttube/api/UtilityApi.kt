e73925b71408780acd2dc3f5a3c74e6077d4c023
Kayra Uylar <k.uylar@outlook.com>
2023-05-15 11:04:50 +0300

    fix(api): fix token refreshing logic
    Fix token refreshing, while deleting multiple token support at the same time
    
    Refs: #1

--- app/src/main/java/dev/kuylar/lighttube/api/UtilityApi.kt
+++ app/src/main/java/dev/kuylar/lighttube/api/UtilityApi.kt
@@ -11,7 +11,9 @@ import java.util.Date
 
 class UtilityApi {
 	companion object {
-		private val tokens = HashMap<String, TokenInfo>()
+		private var refreshToken: String = ""
+		private var accessToken: String = ""
+		private var tokenExpiryTimestamp: Long = 0
 
 		fun getInstances(): ArrayList<LightTubeInstance> {
 			val request: Request = Request.Builder()
@@ -26,32 +28,86 @@ class UtilityApi {
 			}
 		}
 
+		@Throws(Exception::class)
 		fun getToken(host: String, refreshToken: String): String {
-			val key = "$host|$refreshToken"
-			val token =
-				if (tokens.contains(refreshToken)) {
-					tokens[key]!!
-				} else {
-					val t = TokenInfo(refreshToken, host)
-					tokens[key] = t
-					t
-				}
-			token.refreshIfNeeded()
-			return token.accessToken
+			refreshIfNeeded(host, refreshToken)
+			return accessToken
 		}
 
+		@Throws(Exception::class)
 		fun authorizeToken(host: String, refreshToken: String, redirectUri: String): String {
-			val key = "$host|$refreshToken"
-			val token =
-				if (tokens.contains(refreshToken)) {
-					tokens[key]!!
-				} else {
-					val t = TokenInfo(refreshToken, host)
-					tokens[key] = t
-					t
-				}
-			token.authorize(redirectUri)
-			return token.accessToken
+			this.refreshToken = refreshToken
+			authorize(host, redirectUri)
+			return accessToken
+		}
+
+		@Throws(Exception::class)
+		private fun refreshIfNeeded(host: String, refreshToken: String) {
+			if (tokenExpiryTimestamp >= System.currentTimeMillis() / 1000) return
+
+			val body = FormBody.Builder().apply {
+				add("grant_type", "refresh_token")
+				add("refresh_token", refreshToken)
+				add("client_id", "LightTube Android")
+				add("client_secret", "")
+			}.build()
+
+			val request: Request = Request.Builder()
+				.url("$host/oauth2/token")
+				.post(body)
+				.build()
+
+
+			OkHttpClient().newCall(request).execute().use { response ->
+				val res = Gson().fromJson(
+					response.body!!.string(),
+					JsonObject::class.java
+				).asJsonObject
+
+				if (res.has("error"))
+					throw Exception("An error occurred while refreshing token: [${res["error"]}] ${res["error_description"]}")
+
+				tokenExpiryTimestamp =
+					(System.currentTimeMillis() / 1000) + res.getAsJsonPrimitive("expires_in").asLong
+				accessToken = res.getAsJsonPrimitive("access_token").asString
+				Log.d(
+					"UtilityApi",
+					"Refreshed token ${this.refreshToken.take(5)}...${this.refreshToken.takeLast(5)}. It will expire at ${Date(tokenExpiryTimestamp)}"
+				)
+			}
+		}
+
+		@Throws(Exception::class)
+		private fun authorize(host: String, redirectUri: String) {
+			val body = FormBody.Builder().apply {
+				add("grant_type", "authorization_code")
+				add("code", refreshToken)
+				add("client_id", "LightTube Android")
+				add("redirect_uri", redirectUri)
+			}.build()
+
+			val request: Request = Request.Builder()
+				.url("$host/oauth2/token")
+				.post(body)
+				.build()
+
+			OkHttpClient().newCall(request).execute().use { response ->
+				val res = Gson().fromJson(
+					response.body!!.string(),
+					JsonObject::class.java
+				).asJsonObject
+
+				if (res.has("error"))
+					throw Exception("An error occurred while refreshing token: [${res["error"]}] ${res["error_description"]}")
+
+				tokenExpiryTimestamp =
+					(System.currentTimeMillis() / 1000) + res.getAsJsonPrimitive("expires_in").asLong
+				accessToken = res.getAsJsonPrimitive("access_token").asString
+				Log.d(
+					"UtilityApi",
+					"Refreshed token ${refreshToken.take(5)}...${refreshToken.takeLast(5)}. It will expire at $tokenExpiryTimestamp"
+				)
+			}
 		}
 	}
 }
@@ -61,81 +117,3 @@ class LightTubeInstance(
 	val api: Boolean,
 	val accounts: Boolean
 )
-
-class TokenInfo(
-	private val refreshToken: String,
-	private val host: String
-) {
-	lateinit var accessToken: String
-	private var expiryTimestamp = 0L
-	private var refreshed = false
-
-	fun refreshIfNeeded() {
-		if (refreshed) return
-		refreshed = true
-		if (expiryTimestamp >= System.currentTimeMillis() / 1000) return
-
-		val body = FormBody.Builder().apply {
-			add("grant_type", "refresh_token")
-			add("refresh_token", refreshToken)
-			add("client_id", "LightTube Android")
-			add("client_secret", "")
-		}.build()
-
-		val request: Request = Request.Builder()
-			.url("$host/oauth2/token")
-			.post(body)
-			.build()
-
-
-		OkHttpClient().newCall(request).execute().use { response ->
-			val res = Gson().fromJson(
-				response.body!!.string(),
-				JsonObject::class.java
-			).asJsonObject
-
-			if (res.has("error"))
-				throw Exception("An error occurred while refreshing token: [${res["error"]}] ${res["error_description"]}")
-
-			expiryTimestamp =
-				(System.currentTimeMillis() / 1000) + res.getAsJsonPrimitive("expires_in").asLong
-			accessToken = res.getAsJsonPrimitive("access_token").asString
-			Log.d(
-				"UtilityApi",
-				"Refreshed token ${refreshToken.take(5)}...${refreshToken.takeLast(5)}. It will expire at ${Date(expiryTimestamp)}"
-			)
-		}
-	}
-
-	fun authorize(redirectUri: String) {
-		val body = FormBody.Builder().apply {
-			add("grant_type", "authorization_code")
-			add("code", refreshToken)
-			add("client_id", "LightTube Android")
-			add("redirect_uri", redirectUri)
-		}.build()
-
-		val request: Request = Request.Builder()
-			.url("$host/oauth2/token")
-			.post(body)
-			.build()
-
-		OkHttpClient().newCall(request).execute().use { response ->
-			val res = Gson().fromJson(
-				response.body!!.string(),
-				JsonObject::class.java
-			).asJsonObject
-
-			if (res.has("error"))
-				throw Exception("An error occurred while refreshing token: [${res["error"]}] ${res["error_description"]}")
-
-			expiryTimestamp =
-				(System.currentTimeMillis() / 1000) + res.getAsJsonPrimitive("expires_in").asLong
-			accessToken = res.getAsJsonPrimitive("access_token").asString
-			Log.d(
-				"UtilityApi",
-				"Refreshed token ${refreshToken.take(5)}...${refreshToken.takeLast(5)}. It will expire at $expiryTimestamp"
-			)
-		}
-	}
-}
