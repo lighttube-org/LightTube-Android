f7fa671d579963ad69b1f8ad5f5342082cfd6c9a
Kayra Uylar <k.uylar@outlook.com>
2023-05-15 23:09:31 +0300

    feat(app): add crash handler

--- app/src/main/java/dev/kuylar/lighttube/LightTubeApplication.kt
+++ app/src/main/java/dev/kuylar/lighttube/LightTubeApplication.kt
@@ -1,13 +1,24 @@
 package dev.kuylar.lighttube
 
 import android.app.Application
+import android.content.Intent
 import com.google.android.material.color.DynamicColors
+import dev.kuylar.lighttube.ui.activity.CrashHandlerActivity
 
-class LightTubeApplication: Application() {
+
+class LightTubeApplication : Application() {
 	override fun onCreate() {
 		super.onCreate()
 
 		// Apply dynamic color
 		DynamicColors.applyToActivitiesIfAvailable(this)
+
+		// Handle app crashes
+		Thread.setDefaultUncaughtExceptionHandler { _, e ->
+			val intent = Intent(applicationContext, CrashHandlerActivity::class.java)
+			intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			intent.putExtra("trace", e.stackTraceToString())
+			startActivity(intent)
+		}
 	}
 }
